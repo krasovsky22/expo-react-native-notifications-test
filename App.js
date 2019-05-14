c1c5889de64f81244ac4ea0b989aa59d36799d3b
krasovsky <vlad_krasovsky@yahoo.com>
2019-05-14 00:08:36 -0700

    remove bootsrap

--- App.js
+++ App.js
@@ -2,7 +2,9 @@ import React, { Component } from "react";
 import { Text, View } from "react-native";
 import { Link, NativeRouter, Route } from "react-router-native";
 
-import "bootstrap/dist/css/bootstrap.min.css";
+import { ThemeProvider, Header } from "react-native-elements";
+
+const theme = {};
 
 import Home from "./src/view/Home";
 import About from "./src/view/About";
@@ -11,13 +13,19 @@ class App extends Component {
   render() {
     return (
       <NativeRouter>
-        <View>
+        <ThemeProvider theme={theme}>
           <View>
+            <Header
+              placement="left"
+              leftComponent={{ icon: "menu", color: "#fff" }}
+              centerComponent={{ text: "MY TITLE", style: { color: "#fff" } }}
+              rightComponent={{ icon: "home", color: "#fff" }}
+            />
             <Link to="/" underlayColor="#f0f4f7">
               <Text>Home</Text>
             </Link>
             <Link to="/about" underlayColor="#f0f4f7">
-              <Text>About</Text>
+              <Text>About asdasd</Text>
             </Link>
             <Link to="/topics" underlayColor="#f0f4f7">
               <Text>Topics</Text>
@@ -26,7 +34,7 @@ class App extends Component {
 
           <Route exact path="/" component={Home} />
           <Route path="/about" component={About} />
-        </View>
+        </ThemeProvider>
       </NativeRouter>
     );
   }
